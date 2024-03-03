c40ca30fd772671f5354bcb6ba52549860f9056e
VaishnaviSharma7 <sumanzzz7777@gmail.com>
2024-03-03 16:47:53 +0530

    hello

---
+++ module1.mjs
@@ -0,0 +1,7 @@
+const a = "harry"
+const b = "lovely"
+const c = " gunjan"
+
+export default b;
+export {a}
+export {c}
---
+++ module2.mjs
@@ -0,0 +1,3 @@
+import h , {a,c} from "./module1.mjs";
+
+console.log(h , a , c)
